96a9382744bcc478d00d43f9fc0df643895f2676
Rizal <zaril.ziral2020@gmail.com>
2024-11-16 01:32:54 +0700

    main

--- crudDashboard.js
+++ crudDashboard.js
@@ -217,20 +217,20 @@ async function fetchData() {
 const apiUrl = "https://backend-undangan-pernikahan-opang.vercel.app/tamu"; // Replace with your actual API endpoint
 
 // Function to generate invitation URL
-// function generateInvitationUrl(name) {
-//   const formattedName = name.replace(/\s+/g, "+"); // Replace spaces with + and add &partner
-//   return `https://web-wedding-invitation-umber.vercel.app/?to=${formattedName}`;
-// }
-
 function generateInvitationUrl(name) {
-  const formattedName = name
-    .trim()
-    // .replace(/&/g, "&") // Ganti `&` dengan `and`
-    .replace(/\s+/g, "+")
-    .replace(/[^a-zA-Z0-9\-_+]/g, "");
-
+  const formattedName = name.replace(/\s+/g, "+"); // Replace spaces with + and add &partner
   return `https://web-wedding-invitation-umber.vercel.app/?to=${formattedName}`;
 }
+
+// function generateInvitationUrl(name) {
+//   const formattedName = name
+//     .trim()
+//     // .replace(/&/g, "&") // Ganti `&` dengan `and`
+//     .replace(/\s+/g, "+")
+//     .replace(/[^a-zA-Z0-9\-_+]/g, "");
+
+//   return `https://web-wedding-invitation-umber.vercel.app/?to=${formattedName}`;
+// }
 
 // Handle form submission to create the invitation
 document
