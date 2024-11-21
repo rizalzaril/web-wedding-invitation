34b82676509498f769beb2fd38dc97c4da206e0d
Rizal <zaril.ziral2020@gmail.com>
2024-11-21 22:56:09 +0700

    main

--- rsvp.js
+++ rsvp.js
@@ -141,7 +141,7 @@ async function fetchData() {
         "<span class='fa fa-chevron-right fa-xl'></span>", // Right arrow
       ],
       responsive: {
-        0: { items: 2 },
+        0: { items: 1 },
         600: { items: 2 },
         1000: { items: 3 },
       },
