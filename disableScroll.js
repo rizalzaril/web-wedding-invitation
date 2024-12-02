850d3460b694f37e86ca0f66f70688f4b0dbbcd6
Rizal <zaril.ziral2020@gmail.com>
2024-12-02 08:02:25 +0700

    sistem clear contact me if you need update and fix problems

--- disableScroll.js
+++ disableScroll.js
@@ -17,7 +17,8 @@
 // }
 
 // disableScroll();
-
+const song = document.querySelector("#song");
+const audioIcon = document.querySelector("#audio-icon");
 const rootElement = document.querySelector("body");
 let scrollTop, scrollLeft;
 
@@ -32,6 +33,8 @@ function disableScroll() {
   rootElement.style.top = `-${scrollTop}px`;
   rootElement.style.left = `-${scrollLeft}px`;
   rootElement.style.width = "100%";
+  audioIcon.style.display = "none";
+  !playAudio();
 }
 
 function enableScroll() {
@@ -41,13 +44,31 @@ function enableScroll() {
   rootElement.style.top = "";
   rootElement.style.left = "";
   rootElement.style.width = "";
+  audioIcon.style.display = "block";
 
   // Kembalikan ke posisi scroll sebelumnya
   window.scrollTo(scrollLeft, scrollTop);
+  playAudio();
 }
 
-// Panggil disableScroll() untuk menguji
-disableScroll();
+async function playAudio() {
+  song.play();
+}
 
-// // Enable scroll setelah 5 detik untuk demo
-// setTimeout(enableScroll, 5000);
+// // Function to update the icon based on audio state
+
+function toggleAudio() {
+  if (!song.paused) {
+    song.pause();
+    audioIcon.classList.remove("fa-pause");
+    audioIcon.classList.add("fa-play"); // Change icon to play when paused
+    audioIcon.style.animation = "none"; // Remove spinning animation when paused
+  } else {
+    song.play();
+    audioIcon.classList.remove("fa-play");
+    audioIcon.classList.add("fa-pause"); // Change icon to pause when playing
+    audioIcon.style.animation = "spin 2s linear infinite"; // Add spinning animation when playing
+  }
+}
+
+disableScroll();
